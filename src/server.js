b71c4a233159e9383e25bf800252a2cbb42d0f51
Hoang Cao Huan (FPL HN) <huanhcph35218@fpt.edu.vn>
2024-04-11 09:27:06 +0700

    huanhcph35218

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectMongoDB from "./config/dbconfig";
-import router from "./routes";
+import connectMongoDB from "./config/dbconfig.js";
+import router from "./routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -11,7 +11,9 @@ app.use(
     }));
 app.use(express.json());
 
+const port = process.env.PORT || 8000;
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/movies_xuong";
 connectMongoDB(dbUrl)
 app.use("/", router)
-export const viteNodeApp = app;
+// export const viteNodeApp = app;
+app.listen(port, () => console.log("Thành công!"));
